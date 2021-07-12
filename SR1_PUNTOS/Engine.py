d64ec814347ef8d9d909b54a1942af996a48703f
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2021-07-12 17:27:09 -0600

    Update

--- SR1_PUNTOS/Engine.py
+++ SR1_PUNTOS/Engine.py
@@ -5,39 +5,48 @@ height = 500
 
 rend = Renderer(width, height)
 
-rend.glClearColor(0, 0.3, 0)
+rend.glClearColor(1, 1, 1)
 rend.glClear()
 
-rend.glColor(0.2, 1, 1)
+
 contX = 0
 contY = 0
 while contX < 20:
+    rend.glColor(0, 0, 0)
     contX += 1
     # Paredes de la pokebola
     for i in range(100):
         # Lado izquierdo
         rend.glPoint(100+contX, 200+i)
+
         rend.glPoint(-100+contX, 200+i)
-        # Lado derecho
-        # rend.glPoint(380+contX, 200+i)
+
     # Parte de arriba
     for i in range(50):
-        # Lado izquierdo
+
         rend.glPoint(120+contX, 300+i)
         rend.glPoint(-120+contX, 300+i)
-        # Lado derecho
-        #rend.glPoint(360+contX, 300+i)
+        rend.glPoint(120+contX, -350+i)
+        rend.glPoint(-120+contX, -350+i)
+
     # Tapa
     for i in range(40):
-        # Lado izquierdo
+
         rend.glPoint(141+i, 349+contX)
         rend.glPoint(341+i, 349+contX)
         rend.glPoint(181+i, 369+contX)
         rend.glPoint(301+i, 369+contX)
-        # rend.glPoint(340+i, 349+contX)
-    for in in range(60):
+
+        rend.glPoint(141+i, -371+contX)
+        rend.glPoint(341+i, -371+contX)
+        rend.glPoint(181+i, -391+contX)
+        rend.glPoint(301+i, -391+contX)
+    for i in range(80):
+        rend.glPoint(221+i, 389+contX)
+        rend.glPoint(221+i, -411+contX)
 
     print(contX)
-
+rend.glColor(0.8, 0.5, 0.8)
+rend.glPoint(50, 50)
 
 rend.glFinish("output.bmp")
