884e39358319a286374f2a1d2fa4888d84e65b4a
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2021-07-12 15:53:32 -0600

    commit on master

---
+++ SR1_PUNTOS/libraryGame.py
@@ -0,0 +1,30 @@
+import struct
+
+
+def char(c):
+    # 1 byte
+    return struct.pack('=c', c.encode('ascii'))
+
+
+def word(w):
+    # 2 bytes
+    return struct.pack('=h', w)
+
+
+def dword(d):
+    # 4 bytes
+    return struct.pack('=l', d)
+
+
+def color(r, g, b):
+    # Acepta valores de 0 a 1
+    return bytes([int(b*255), int(g*255), int(r*255)])
+
+
+class Renderer(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def function(self):
+        self.width
