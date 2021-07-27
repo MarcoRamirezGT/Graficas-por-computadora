3e8fb9c81e95fecd9a0cde6d7b18c00a4271c957
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2021-07-26 20:37:54 -0600

    Update

--- Lab01/Engine.py
+++ Lab01/Engine.py
@@ -1,36 +1,35 @@
 from libraryGame import Renderer, V2
 
-width = 500
-height = 500
-
+width = 1920
+height = 1080
 
 rend = Renderer(width, height)
 Poligono1 = [(V2(165, 380)), (V2(185, 360)), (V2(180, 330)), (V2(207, 345)), (V2(233, 330)),
              (V2(230, 360)), (V2(250, 380)), (V2(220, 385)), (V2(205, 410)), (V2(193, 383))]
 
-
-rend.glLine(Poligono1[0], Poligono1[1])
-rend.glLine(Poligono1[1], Poligono1[2])
-rend.glLine(Poligono1[2], Poligono1[3])
-rend.glLine(Poligono1[3], Poligono1[4])
-rend.glLine(Poligono1[4], Poligono1[5])
-rend.glLine(Poligono1[5], Poligono1[6])
-rend.glLine(Poligono1[6], Poligono1[7])
-rend.glLine(Poligono1[7], Poligono1[8])
-rend.glLine(Poligono1[8], Poligono1[9])
-rend.glLine(Poligono1[9], Poligono1[0])
+for x in range(len(Poligono1)):
+    print(x)
+    rend.glLine(Poligono1[x], Poligono1[(x+1) % len(Poligono1)])
 
 
 Poligono2 = [(V2(321, 335)), (V2(288, 286)), (V2(339, 251)), (V2(374, 302))]
 
-rend.glLine(Poligono2[0], Poligono2[1])
-rend.glLine(Poligono2[1], Poligono2[2])
-rend.glLine(Poligono2[2], Poligono2[3])
-rend.glLine(Poligono2[3], Poligono2[0])
+for x in range(len(Poligono2)):
+    rend.glLine(Poligono2[x], Poligono2[(x+1) % len(Poligono2)])
 
-Poligono3 = [
-    (377, 249)(411, 197)(436, 249)]
+Poligono3 = [(V2(377, 249)), (V2(411, 197)), (V2(436, 249))]
+for x in range(len(Poligono3)):
+    rend.glLine(Poligono3[x], Poligono3[(x+1) % len(Poligono3)])
 
+
+Poligono4 = [(V2(413, 177)), (V2(448, 159)), (V2(502, 88)), (V2(553, 53)), (V2(535, 36)), (V2(676, 37)), (V2(660, 52)),
+             (V2(750, 145)), (V2(761, 179)), (V2(672, 192)), (V2(659, 214)
+                                                              ), (V2(615, 214)), (V2(632, 230)), (V2(580, 230)),
+             (V2(597, 215)), (V2(552, 214)), (V2(517, 144)), (V2(466, 180))
+             ]
+
+for x in range(len(Poligono4)):
+    rend.glLine(Poligono4[x], Poligono4[(x+1) % len(Poligono4)])
 
 # for x in range(165):
 # rend.glPoint((x*x+1)//2, (x*x+1)//2)
