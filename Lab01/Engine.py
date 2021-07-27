9cd8ffaba410c2828f6afb91e5e66a23a7536938
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2021-07-26 21:04:10 -0600

    Update

--- Lab01/Engine.py
+++ Lab01/Engine.py
@@ -1,4 +1,6 @@
 from libraryGame import Renderer, V2
+import sys
+import random
 
 width = 1920
 height = 1080
@@ -9,7 +11,11 @@ Poligono1 = [(V2(165, 380)), (V2(185, 360)), (V2(180, 330)), (V2(207, 345)), (V2
 
 for x in range(len(Poligono1)):
     print(x)
+    print(Poligono1[x])
+
     rend.glLine(Poligono1[x], Poligono1[(x+1) % len(Poligono1)])
+    for y in range(3):
+        rend.glLine((V2(165+y, 380-y)), (V2(185+y, 360+y)))
 
 
 Poligono2 = [(V2(321, 335)), (V2(288, 286)), (V2(339, 251)), (V2(374, 302))]
@@ -31,7 +37,11 @@ Poligono4 = [(V2(413, 177)), (V2(448, 159)), (V2(502, 88)), (V2(553, 53)), (V2(5
 for x in range(len(Poligono4)):
     rend.glLine(Poligono4[x], Poligono4[(x+1) % len(Poligono4)])
 
-# for x in range(165):
-# rend.glPoint((x*x+1)//2, (x*x+1)//2)
+Poligono5 = [(V2(682, 175)), (V2(708, 120)), (V2(735, 148)), (V2(739, 170))]
+
+
+for x in range(len(Poligono5)):
+    rend.glLine(Poligono5[x], Poligono5[(x+1) % len(Poligono5)])
+
 
 rend.glFinish("Lab01.bmp")
